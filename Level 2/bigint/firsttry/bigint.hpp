0dfe5360af3002b7579f4f2f8bd4440cdbe13cc5
Bendix-Schmidt <bendix.schmidt2000@gmail.com>
2025-02-28 18:40:00 +0100

    more learning

--- Level 2/bigint/firsttry/bigint.hpp
+++ Level 2/bigint/firsttry/bigint.hpp
@@ -7,18 +7,16 @@ class bigint
 {
 	private:
 		std::string num;
-	
+
 	public:
-		bigint(size_t n) : num(std::to_string(n)){}
-		bigint() : num("0"){}
-
-		bigint(const bigint &original) : num(original.num){}
-
+		bigint() : num("0") {}
+		bigint(size_t n) : num(std::to_string(n)) {}
+		bigint(const bigint &oth) : num(oth.num) {}
 		bigint(const std::string &n)
 		{
-			if (n.empty() || !std::all_of(n.begin(), n.end(), ::isdigit) || (n.size() > 1 && n[0] == '0'))
+			if (n.empty() || !std::all_of(n.begin(), n.end(), ::isdigit) || n.size() > 1 && n[0] == '0')
 			{
-				std::cout << "Invalid number string" << std::endl;
+				std::cout << "invalid string" << std::endl;
 				num = "0";
 			}
 			else
@@ -27,9 +25,10 @@ class bigint
 			}
 		}
 
-		friend std::ostream& operator<<(std::ostream &os, const bigint &b)
+		friend std::ostream &operator<<(std::ostream &os, bigint &b)
 		{
-			return (os << b.num);
+			os << b.num;
+			return (os);
 		}
 
 		std::string add_strings(const std::string &n1, const std::string &n2) const
@@ -40,7 +39,7 @@ class bigint
 			int j = n2.size() - 1;
 			while (i >= 0 || j >= 0 || carry)
 			{
-				int sum = (i >= 0 ? n1[i--] - '0' : 0) + (j >=0 ? n2[j--] - '0' : 0) + carry;
+				int sum = (i >= 0 ? n1[i--] - '0' : 0) + (j >= 0 ? n2[j--] - '0' : 0) + carry;
 				result.push_back(sum % 10 + '0');
 				carry = sum / 10;
 			}
@@ -48,7 +47,19 @@ class bigint
 			return (result);
 		}
 
-		bigint operator+(const bigint& oth) const {
-			return bigint(add_strings(num, oth.num));
+		bigint operator+(const bigint &oth) const
+		{
+			return (bigint(add_strings(num, oth.num)));
 		}
+
+
+
+
+
+
+
+
+
+
+
 };
